7c69856a652c8b4cb8f8c2854638ed67aff794e1
Shen Siwei <shensiwei@youku.com>
2013-12-29 17:36:10 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'mysql2' #,'0.2.6'
+gem 'rubycas-client', '2.3.9'
+gem 'jbuilder', '~> 1.2'
+gem "simple-navigation"
+gem 'thin'
+
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :test, :development do
+  gem "rspec-rails", ">= 2.5.0"
+  gem 'magic_encoding', '0.0.2'
+  gem 'capistrano', '2.12.0'
+  gem 'capistrano-rbenv', '1.0.1'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OldPhotos::Application.config.secret_key_base = '4e0c8ad0c5a7ac6af60763be65321af80dce72e096ab6182d0db08346b3215fd7700f6697b9b4c573441a4df16afd0e8f65a4d8a1f58418abd0d3fb554eab9f6'
