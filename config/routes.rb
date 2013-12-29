b3d56523b18591c4a45f637c99dfa374e667768f
Shen Siwei <sg552sg552@gmail.com>
2013-12-29 18:27:25 +0800

    added assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 OldPhotos::Application.routes.draw do
+  root :to => 'photos#index'
   resources :members
-
   resources :comments
-
   resources :users
-
   resources :organizations
   resources :photos
 end
